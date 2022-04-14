679d49461adec4c97ca6bfac7fa81ab1a2c827da
afeiship <1290657123@qq.com>
2022-04-14 23:27:17 +0800

    feat: udpate prj desc

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,40 +1,32 @@
-'use strict';
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
+"use strict";
+const Generator = require("yeoman-generator");
+const chalk = require("chalk");
+const yosay = require("yosay");
+const globby = require("globby");
+const yoHelper = require("@jswork/yeoman-generator-helper");
+const getp = require("@jswork/generator-prompts");
+const prompts = getp(["scope", "registry", "project_name", "description"]);
 
 module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
     this.log(
-      yosay(
-        `Welcome to the rad ${chalk.red('generator-backup')} generator!`
-      )
+      yosay(`Welcome to the rad ${chalk.red("generator-backup")} generator!`)
     );
 
-    const prompts = [
-      {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true
-      }
-    ];
-
     return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
       this.props = props;
+      yoHelper.rewriteProps(props, {
+        exclude: ["email", "description", "author", "homepage", "registry"]
+      });
     });
   }
 
   writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+    this.fs.copyTpl(
+      globby.sync(this.templatePath("**"), { dot: true }),
+      this.destinationPath(),
+      this.props
     );
-  }
-
-  install() {
-    this.installDependencies();
   }
 };
